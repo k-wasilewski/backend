659ce98c7dc2ab87382978dee3a200c199d02f9c
k-wasilewski <k.k.wasilewski@gmail.com>
2020-05-26 21:02:03 +0200

    regex: deconstructing frontend response done

--- src/main/java/com/semantive/backend/controllers/AddOrdersController.java
+++ src/main/java/com/semantive/backend/controllers/AddOrdersController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 public class AddOrdersController {
@@ -14,9 +16,26 @@ public class AddOrdersController {
     @CrossOrigin(origins = "http://localhost:3000")
     public String addOrders(@RequestParam("name") String name, @RequestParam("age") int age,
                             @RequestParam("orders") String orders) {
-        String[] ordersArr = orders.split("(?<=[0-9])(?=[0-9])|(?<=[0-9])(?= )");
-        System.out.println(orders);
-        System.out.println(Arrays.toString(ordersArr));
+        String[] ordersArr = orders.split("(?=[0-9])");
+        Pattern idRegex = Pattern.compile("^(\\d),");
+        Pattern colorRegex = Pattern.compile(",(.*),");
+        Pattern sizeRegex = Pattern.compile(",(\\w)$");
+
+        System.out.println("array"+Arrays.toString(ordersArr));
+
+        for (String order : ordersArr) {
+            if (order.charAt(order.length()-1)==',') {
+                order = order.substring(0, order.length() - 1);
+            }
+            System.out.println("order: "+order);
+            Matcher idMatcher = idRegex.matcher(order);
+            Matcher colorMatcher = colorRegex.matcher(order);
+            Matcher sizeMatcher = sizeRegex.matcher(order);
+
+            while (idMatcher.find()) System.out.println("id="+idMatcher.group(1));
+            while (colorMatcher.find()) System.out.println("color="+colorMatcher.group(1));
+            while (sizeMatcher.find()) System.out.println("size="+sizeMatcher.group(1));
+        }
 
         return "Zamówienie dla ["+name+", "+age+"] zostało przyjęte";
     }
